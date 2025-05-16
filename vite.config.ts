a1f78d36083b9e0817693b4c64e03e15dfb0d82e
Siddharth Mehta <66232143+siddharth270@users.noreply.github.com>
2025-05-16 22:10:19 +0530

    Auto: Final commit before deployment

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 700, // Optional: raise the warning threshold
+  },
 }));
