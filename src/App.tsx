21e7da90d04f006ecc0f31fd017f04500bc0b1e8
Siddharth Mehta <66232143+siddharth270@users.noreply.github.com>
2025-05-18 20:55:29 +0530

    Auto: Final commit before deployment

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
           {/* <Route path="/products/m3doc" element={<ProductM3DOC />} /> */}
           <Route path="/products/diagnostic-microscope" element={<ProductDiagnosticMicroscope />} />
           <Route path="/products/lumora-scan" element={<ProductLumoraScan />} />
-          <Route path="about" element={<About />} />
+          <Route path="/about" element={<About />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<About />} />
         </Routes>
