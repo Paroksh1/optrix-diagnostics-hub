36b9f24e2a3dd1c36a7e47be676a1b9ab1340d68
Siddharth Mehta <66232143+siddharth270@users.noreply.github.com>
2025-05-18 21:16:21 +0530

    Auto: Final commit before deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,15 +10,18 @@ import NotFound from "./pages/NotFound";
 import ProductDiagnosticMicroscope from "./pages/ProductDiagnosticMicroscope";
 import ProductLumoraScan from "./pages/ProductLumoraScan";
 import About from "./pages/About";
+import { HashRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+
+
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* Commented out M3DOC route as requested */}
@@ -29,9 +32,11 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<About />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+        </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+
+  
 );
 
 export default App;
